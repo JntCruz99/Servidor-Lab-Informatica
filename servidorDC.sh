ebc79e9bff63c96eaec4c8537fc30456fd2d3972
Jonatas Cruz <131266716+JntCruz99@users.noreply.github.com>
2023-12-14 15:58:44 -0300

    Atualizar o servidorDC.sh

--- servidorDC.sh
+++ servidorDC.sh
@@ -1,2 +1,1 @@
-sudo apt install autoconf bind9utils bison debhelper dnsutils docbook-xml
-docbook-xsl flex gdb libjansson-dev libacl1-dev libaio-dev libarchive-dev libattr1-dev libblkid-dev libbsd-dev libcap-dev libcups2-dev libgnutls28-dev libgpgme11-dev libjson-perl libldap2-dev libncurses5-dev libpam0g-dev libparse-yapp-perl libpopt-dev libreadline-dev nettle-dev perl perl-modules pkg-config python-all-dev python-crypto python-dbg python-dev python-dnspython python3-dnspython python-gpg python3-gpg python-markdown python3-markdown python3-dev xsltproc zlib1g-dev liblmdb-dev lmdb-utils libsystemd-dev
+sudo apt install samba krb5-user winbind libnss-winbind smbclient ldap-utils acl attr ntp
